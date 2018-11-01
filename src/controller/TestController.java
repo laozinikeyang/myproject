0ae2434847d7a4549f409d200b8e77b5c5fc4084
lepu <lepu@LEPU-20181101BA>
2018-11-01 21:17:52 +0800

    123

--- src/controller/TestController.java
+++ src/controller/TestController.java
@@ -1,5 +1,8 @@
 package controller;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.apache.shiro.authz.annotation.RequiresUser;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpRequest;
@@ -7,6 +10,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import entity.QuestionnaireAnswer;
 import service.TestService;
@@ -25,9 +29,12 @@ public class TestController {
 		this.service = service;
 	}
 	@RequestMapping(value="test.spring")
-	public String test(Model model) {
+	public ModelAndView test(ModelAndView mav,String abc) {
+		System.out.println("abc:"+abc);
 		QuestionnaireAnswer questionnaireAnswer =service.selectByPrimaryKey("03a2173c-7a3e-498f-be34-359ae4a6ce4c");
 		System.out.println(questionnaireAnswer.getAnswerId());
-		return "index";
+		mav.addObject("bb", questionnaireAnswer);
+		mav.setViewName("error");;
+		return mav;
 	}
 }
