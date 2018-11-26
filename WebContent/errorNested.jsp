8ca15c3f3c89f05471f73b282522d1cb1c863089
lepu <lepu@LEPU-20180713JN>
2018-11-26 16:28:52 +0800

    2018-11-26 16:28:50

---
+++ WebContent/errorNested.jsp
@@ -0,0 +1,51 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+    <%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<%
+String path = request.getContextPath();
+String basePath = request.getScheme()+"://"+request.getServerName()+":"+request.getServerPort()+path+"/";
+String socketPath = request.getServerName()+":"+request.getServerPort()+path+"/";
+%>
+<!DOCTYPE html>
+<html>
+  <head>
+    meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width,initial-scale=1">
+<title>Insert title here</title>
+<link href="<%=basePath%>bootstrap-3.3.7-dist/css/bootstrap.min.css" rel="stylesheet">
+<link rel="<%=basePath%>stylesheet" href="css/thinksite.css">
+<script src="<%=basePath%>js/html5shiv.min.js"></script>
+<script src="<%=basePath%>js/respond.min.js"></script>
+<script src="<%=basePath%>jedate/jedate.js"></script>
+  </head>
+<body style="background-color: #fefefe;">
+	<!-- 这是导航条 -->
+	<jsp:include page="./WEB-INF/jsp/menu.jsp"></jsp:include>
+
+
+	<div class="container" style="padding-bottom: 70px;">
+		图片太大了
+	</div>
+	
+	<footer class="footer navbar-fixed-bottom ">
+	    <div class="container" style="background-color: #000000">
+	    	<div class="row">
+	                <div class="col-sm-12">
+	                    <span><a href="http://www.mrkj.com/">明日科技</a></span> | 
+	                    <span>Copyright &copy; <a href="http://www.mrkj.com/">吉林省明日科技有限公司</a></span> | 
+	                    <span>吉ICP备16003039号-1</span>
+	                    <span>站长QQ:80303857</span>
+	                </div>
+	            </div>
+	    </div>
+	</footer>
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script type="text/javascript">
+		var basePath = '<%=basePath%>';
+		var socketPath = '<%=socketPath%>';
+	</script>
+    <script src="<%=basePath%>js/jquery.min.js"></script>
+	<script src="<%=basePath%>bootstrap-3.3.7-dist/js/bootstrap.min.js"></script>
+</body>
+</html>
--- src/controller/TestController.java
+++ src/controller/TestController.java
@@ -21,8 +21,18 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import org.apache.commons.io.FileUtils;
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.DisabledAccountException;
+import org.apache.shiro.authc.ExcessiveAttemptsException;
+import org.apache.shiro.authc.ExpiredCredentialsException;
+import org.apache.shiro.authc.IncorrectCredentialsException;
+import org.apache.shiro.authc.LockedAccountException;
+import org.apache.shiro.authc.UnknownAccountException;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.authz.UnauthorizedException;
 import org.apache.shiro.authz.annotation.RequiresAuthentication;
 import org.apache.shiro.authz.annotation.RequiresUser;
+import org.apache.shiro.subject.Subject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpRequest;
 import org.springframework.stereotype.Controller;
@@ -36,6 +46,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.multipart.MultipartHttpServletRequest;
 import org.springframework.web.servlet.ModelAndView;
+
 import dao.SysLoginMapper;
 import dao.SysRoleMapper;
 import entity.*;
@@ -44,6 +55,7 @@ import servicedao.Sys_permissionService;
 import servicedao.Sys_permissionTreeService;
 import servicedao.Sys_role_permissionService;
 import standard.Util;
+import utils.MD5;
 @Transactional
 @Controller
 public class TestController {
@@ -64,6 +76,71 @@ public class TestController {
 	Sys_permissionTreeService perTreeService;
 	@Resource
 	Sys_role_permissionService sysRolPerService;
+	
+	
+	
+	@RequestMapping(value="login/verification.spring")
+	public ModelAndView login(HttpServletRequest request,String username , String password ,@RequestParam(defaultValue="0000") String verifyCode, Model model) {
+		System.out.println("aaaaaaaaaaaaa");
+		ModelAndView mav = new ModelAndView();
+		String msg = "";
+		HttpSession session = request.getSession();
+		String SessionverifyCode = "0000";//(String)session.getAttribute("verifyCode");
+		if (SessionverifyCode!=null&&SessionverifyCode.equals(verifyCode)){
+			session.setAttribute("verifyCode", MD5.md5(Math.random()+""));
+			UsernamePasswordToken token = new UsernamePasswordToken(username, password);
+			//token.setRememberMe(true);
+			Subject subject = SecurityUtils.getSubject();
+			try{
+				subject.login(token);
+				SysLogin loginEntity = userloginService.selectByUsername(username);
+				session.setAttribute("UserName", username);
+				session.setAttribute("loginEntity", loginEntity);
+				mav.setViewName("redirect:/index");
+			}catch(IncorrectCredentialsException e) {  
+		        msg = "登录密码错误. Password for account " + token.getPrincipal() + " was incorrect.";  
+		        model.addAttribute("message", msg);  
+		        System.out.println(msg);  
+		        mav.setViewName("redirect:/login.jsp");
+		    } catch (ExcessiveAttemptsException e) {  
+		        msg = "登录失败次数过多";  
+		        model.addAttribute("message", msg);  
+		        System.out.println(msg);  
+		        mav.setViewName("redirect:/login.jsp");
+		    } catch (LockedAccountException e) {  
+		        msg = "帐号已被锁定. The account for username " + token.getPrincipal() + " was locked.";  
+		        model.addAttribute("message", msg);  
+		        System.out.println(msg);  
+		        mav.setViewName("redirect:/login.jsp");
+		    } catch (DisabledAccountException e) {  
+		        msg = "帐号已被禁用. The account for username " + token.getPrincipal() + " was disabled.";  
+		        model.addAttribute("message", msg);  
+		        System.out.println(msg);  
+		        mav.setViewName("redirect:/login.jsp");
+		    } catch (ExpiredCredentialsException e) {  
+		        msg = "帐号已过期. the account for username " + token.getPrincipal() + "  was expired.";  
+		        model.addAttribute("message", msg);  
+		        System.out.println(msg);  
+		        mav.setViewName("redirect:/login.jsp");
+		    } catch (UnknownAccountException e) {  
+		        msg = "帐号不存在. There is no user with username of " + token.getPrincipal();  
+		        model.addAttribute("message", msg);  
+		        System.out.println(msg);  
+		        mav.setViewName("redirect:/login.jsp");
+		    } catch (UnauthorizedException e) {  
+		        msg = "您没有得到相应的授权！" + e.getMessage();  
+		        model.addAttribute("message", msg);  
+		        System.out.println(msg);  
+		        mav.setViewName("redirect:/login.jsp");
+		    }
+		}else{
+			mav.addObject("msg", "验证码错误！");
+			mav.setViewName("redirect:/login.jsp");
+		}
+		return mav;
+	}
+	
+	
 	
 	
 	@RequestMapping("per/edit.spring")
